41a5428f180e54475297ab2a5c6dfa2b3850d595
romcol <e.rcolombo2@gmail.com>
2025-11-05 21:20:39 -0300

    feat: Initial setup

--- mem_sim.py
+++ mem_sim.py
@@ -1,4 +1,5 @@
 import collections
+from typing import Dict, List, Tuple
 
 class MemorySimulator:
     def __init__(self, page_size, num_tlb_entries, num_frames, rep_policy):
@@ -10,9 +11,9 @@ class MemorySimulator:
         if rep_policy not in ['LRU', 'SecondChance']:
             raise ValueError("Política de substituição inválida. Use 'LRU' ou 'SecondChance'.")
 
-        self.tlb = []  
-        self.page_table = {} 
-        self.frames = [None] * num_frames
+        self.tlb: collections.OrderedDict = collections.OrderedDict()
+        self.page_table: Dict[int, int] = {}
+        self.frames: List[Tuple[int, int]] = [None] * num_frames
 
         # contadores de estatísticas
         self.tlb_hits = 0
@@ -20,12 +21,48 @@ class MemorySimulator:
         self.page_faults = 0
 
 
-    def access_memory(self, virtual_address):
+    def access_memory(self, virtual_address: int) -> None:
         """
         Simula o acesso a um endereço virtual.
         Deve atualizar os contadores e aplicar a política de substituição se necessário.
         """
         
+        page_number = virtual_address // self.page_size
+        # offset = virtual_address % self.page_size (not used in this simulation)
+
+        # Verify TLB:
+        if page_number in self.tlb:
+            self.tlb_hits += 1
+            frame_number = self.tlb[page_number]
+            self.tlb.move_to_end(page_number) # Updates order for LRU
+            return
+        
+        self.tlb_misses += 1
+
+        # Verify Page Table:
+        if page_number in self.page_table:
+            frame_number = self.page_table[page_number]
+            self._update_tlb(page_number, frame_number)
+            return
+
+        self.page_faults += 1
+        frame_number = self._handle_page_fault(page_number)
+        self._update_tlb(page_number, frame_number)
+        
+    def _update_tlb(self, page_number: int, frame_number: int) -> None:
+        """
+        Inserts/updates an entry in the TLB using LRU policy.
+        """
+        
+        if page_number in self.tlb:
+             self.tlb.move_to_end(page_number)
+        else:
+            if len(self.tlb) >= self.num_tlb_entries:
+                self.tlb.popitem(last=False) # Remove the least recently used entry
+            self.tlb[page_number] = frame_number
+    
+    def _handle_page_fault(self, page_number: int) -> int:
+        pass
 
     def print_statistics(self):
         print("=" * 60)
